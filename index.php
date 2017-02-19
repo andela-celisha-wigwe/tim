883e37002f26d94f162729000cc7d1beacf5f773
andela-celisha-wigwe <chijioke.elisha-wigwe@andela.com>
2017-02-19 10:32:45 +0100

    add google tracking and modify title

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+	<title>MIT CGPA Calculator</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<meta charset="UTF-8">
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.6/handlebars.min.js" ></script>
@@ -136,7 +137,16 @@
 		}
 
 	</script>
-	<title>Document</title>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-92192279-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body onload="displayForm()">
 	<script type="text/x-handlebars-template" id="course-template">
